3f61dc71b185ae2009121f72d1ad21c06fc18e56
Rafael M Cunha <me@rafaelmc.net>
2024-12-05 01:06:15 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,31 @@ const FATAL_ERROR: &str = "Failed to execute git command";
 
 fn main() {
     check_is_git_repository();
-    let empty = check_if_empty();
-    if empty {
+    if check_if_empty() {
         create_initial_commit();
     }
+    if check_if_clean() {
+        eprintln!("Repository is clean, nothing to do");
+        std::process::exit(0);
+    }
 
-    //check_if_clean()
-    // TODO: Check if repository is clean
-    // TODO: Check if it's the first commit
-    // TODO: Check if the repository is clean
+    let output = Command::new("git")
+        .arg("rev-list")
+        .arg("--all")
+        .arg("--count")
+        .output()
+        .expect(FATAL_ERROR);
+    let count = String::from_utf8(output.stdout)
+        .expect("Failed to parse git rev-list output")
+        .trim()
+        .replace("\n", "")
+        .parse::<i32>()
+        .expect("Failed to parse git rev-list output");
+    let count = count + 1;
+
+    commit(&count.to_string());
+
+
 }
 
 fn check_is_git_repository() {
@@ -30,12 +46,10 @@ fn check_is_git_repository() {
 
 fn check_if_empty() -> bool {
     let command = Command::new("git").arg("count-objects").output().expect(FATAL_ERROR);
-    let output = String::from_utf8(command.stdout).expect("Failed to parse git count-objects output");
-    let count = output.split_whitespace().nth(0).expect("Failed to parse git count-objects output");
+    let output = String::from_utf8(command.stdout)
+        .expect("Failed to parse git count-objects output");
+    let count = output.split_whitespace().next().expect("Failed to parse git count-objects output");
 
-
-
-    println!("count: {}", count);
     count == "0"
 }
 
@@ -53,17 +67,36 @@ fn git_add_all() {
 }
 
 fn create_initial_commit() {
+    commit("1")
+}
+
+fn commit(message: &str) {
     git_add_all();
 
     let output = Command::new("git")
         .arg("commit")
         .arg("-m")
-        .arg("1")
+        .arg(message)
         .output()
         .expect(FATAL_ERROR);
 
     if !output.status.success() {
-        eprintln!("Failed to create initial commit");
+        eprintln!("Failed to create commit");
         std::process::exit(1);
     }
+}
+
+fn check_if_clean() -> bool {
+    let output = Command::new("git")
+        .arg("status")
+        .arg("--porcelain")
+        .output()
+        .expect(FATAL_ERROR);
+
+    if !output.status.success() {
+        eprintln!("Failed to check if the repository is clean");
+        std::process::exit(1);
+    }
+
+    String::from_utf8(output.stdout).expect("Failed to parse git status output").is_empty()
 }
