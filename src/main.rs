f20ac2fb339edf63a6703f4188965c434c719fb1
Rafael M Cunha <me@rafaelmc.net>
2024-12-07 21:48:37 +0100

    9 - I think dashes can work better

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,7 @@ fn main() {
     if config.commit_message.is_empty() {
         message = count.to_string();
     } else {
-        message = format!("{}: {}", count, config.commit_message);
+        message = format!("{} - {}", count, config.commit_message);
     }
 
     commit(&message);
